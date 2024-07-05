5182806e1c4a1d117307432a162b5b58a3d2dd90
unknown <okdiphala@hotmail>
2024-07-05 17:54:11 +0200

    Made some initial changes to the file

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool quit = false;
+
+        while (!quit)
+        {
+        
+            Console.Clear()
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1.Start Breathing Activity ");
+            Console.WriteLine("2.Start Reflecting Activity ");
+            Console.WriteLine("3.Start Listing Activity ");
+            Console.WriteLine("4.Quit ");
+            
+            Console.Write("Select a choice from the menu: ");
+            string input = Console.ReadLine();
+
+            switch (input)   
+            {
+                case "1":
+                    var breathingactivity = new BreathingActivity();
+                    breathingactivity.Execute();
+                    break;
+
+                case "4":
+                    var reflectingactivity = new ReflectingActivity();
+                    reflectingactivity.Execute();
+                    break;
+
+                case "3":
+                    var listingactivity = new ListingActivity();
+                    listingactivity.Execute();
+                    break;
+
+                case "4":
+                    quit = true;
+                    Console.WriteLine();
+                    break;
+                default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+            }
+  
+            if (!quit)
+            {
+                Console.WriteLine("Enter any key to return to the main menu");
+                Console.ReadKey();
+            }
+        }
     }
 }
---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,26 @@
+public class ReflectingActivity
+{
+    public void Display()
+    {
+        Console.WriteLine("Welcome to the Reflecting Activity.");
+        Console.WriteLine();
+        Console.WriteLine("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life");
+        Console.WriteLine();
+    }
+
+    public void PromptUserInput()
+    {
+        Console.WriteLine("How long, in seconds, would you like for your session?");
+        string userInput = Console.ReadLine();
+    }
+
+    public void NextScreen()
+    {
+        Console.WriteLine("Get ready...");
+        Console.WriteLine();
+        Console.WriteLine("Consider the following prompt:");
+
+    }
+
+    public  void 
+}

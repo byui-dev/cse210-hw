8a49ec7fb1c3fa5c6f4bbbd9c9bfe42385fb4cea
unknown <okdiphala@hotmail>
2024-07-12 18:04:54 +0200

    Made changes to the file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,78 +1,60 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
-        Console.WriteLine("Please select one of the following choices");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.WriteLine("What would you like to do? ");
-
-    }
-} 
-=======
-        Journal journal = new Journal();
-        PromptGenerator promptGenerator = new PromptGenerator();
-        bool running = true;
+        Journal journal = new Journal(); // Create a new Journal object
+        PromptGenerator promptGenerator = new PromptGenerator(); // Create a new PromptGenerator object
+        bool running = true; // Boolean variable to control the loop
 
         while (running)
         {
-            Console.WriteLine("Welcome to the Journal Program! ");
-            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
             Console.Write("What would you like to do? ");
-         
 
-            string choice = Console.ReadLine();
-            switch(choice)
+            string choice = Console.ReadLine(); // Read user input
+            switch (choice)
             {
                 case "1":
-                    string prompt = promptGenerator.GetRandomPrompt();
-                    Console.WriteLine(prompt);
-                    string response = Console.ReadLine();
-                    journal.AddEntry(new Entry(prompt, response));
+                    string prompt = promptGenerator.GetRandomPrompt(); // Get a random prompt
+                    Console.WriteLine(prompt); // Display the prompt
+                    string response = Console.ReadLine(); // Read the user's response
+                    journal.AddEntry(new Entry(prompt, response)); // Add the new entry to the journal
                     break;
-        
+
                 case "2":
-                    journal.DisplayJournal();
+                    journal.DisplayJournal(); // Display all journal entries
                     break;
 
                 case "3":
                     Console.Write("Enter filename to load: ");
-                    string saveFilename = Console.ReadLine();
-                    journal.SaveJournal(saveFilename);
+                    string loadFilename = Console.ReadLine(); // Read the filename to load
+                    journal.LoadJournal(loadFilename); // Load the journal from the file
                     break;
 
-                case "4":    
-                   Console.Write("Enter filename to save: ");
-                   string loadFilename = Console.ReadLine();
-                   journal.LoadJournal(loadFilename);
-                   break;
+                case "4":
+                    Console.Write("Enter filename to save: ");
+                    string saveFilename = Console.ReadLine(); // Read the filename to save
+                    journal.SaveJournal(saveFilename); // Save the journal to the file
+                    break;
 
                 case "5":
-                   running = false;
-                   break;
+                    running = false; // Exit the loop and end the program
+                    break;
 
                 default:
-                   Console.WriteLine("Invalid option. Please try again.");
-                   break;
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
             }
         }
     }
-}               
-
-
-
-    
-    
-    
->>>>>>> dac1d97f751bcf5f0c96067fcda85ec086f9d003
+}
---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace GoalApp
+{
+    public class EternalGoal : Goal
+    {
+        public EternalGoal()
+        {
+        }
+
+        public override void RecordEvent()
+        {
+            Console.WriteLine("what is the name of your goal?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is a short description of it?");
+            string description = Console.ReadLine();
+            Console.WriteLine("what is the amount of points associated with this goal?");
+            string points = Console.ReadLine();
+            int.TryParse(points);
+
+        }
+
+        public override bool IsComplete()
+        {
+
+        }
+            
+        public override string GetStringRepresentation()
+        {
+            string GetStringRepresentation = Console.ReadLine($"[ ] name (description)");
+        }
+    }
+}        
+
+        
